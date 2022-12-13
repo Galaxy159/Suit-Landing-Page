21282c9f13dd93eb64a4c06f0ab4b4ddd34dbf97
Galaxy159 <braun.asaf@gmail.com>
2022-12-13 12:34:47 +0200

    initial file setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./styles/App.css";
+import Topbar from "./components/Topbar";
+import Header from "./components/Header";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <>
+      <Topbar />
+      <Header />
+      <Testimonial />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
