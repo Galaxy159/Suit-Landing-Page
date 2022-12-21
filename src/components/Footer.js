7911af404184f6f0e89348512b2280da478ba0d1
Galaxy159 <braun.asaf@gmail.com>
2022-12-21 13:16:33 +0200

    tablet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import logo from "../assets/logo.svg";
-import facebook from "../assets/icon-facebook.svg";
-import twitter from "../assets/icon-twitter.svg";
-import instagram from "../assets/icon-instagram.svg";
+import sprite from "../assets/sprite.svg";
 
 export default function Footer() {
   return (
@@ -14,27 +12,15 @@ export default function Footer() {
           <p className="footer__copyright--text">Copyright - Suite</p>
         </div>
         <div className="footer__social">
-          <div className="footer__social--icon">
-            <img
-              src={facebook}
-              alt="Facebook"
-              className="footer__social--icon-img"
-            />
-          </div>
-          <div className="footer__social--icon">
-            <img
-              src={twitter}
-              alt="Twitter"
-              className="footer__social--icon-img"
-            />
-          </div>
-          <div className="footer__social--icon">
-            <img
-              src={instagram}
-              alt="Instagram"
-              className="footer__social--icon-img"
-            />
-          </div>
+          <svg className="footer__social--icon">
+            <use href={`${sprite}#icon-facebook`} />
+          </svg>
+          <svg className="footer__social--icon">
+            <use href={`${sprite}#icon-instagram`} />
+          </svg>
+          <svg className="footer__social--icon">
+            <use href={`${sprite}#icon-twitter`} />
+          </svg>
         </div>
       </div>
     </>
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,27 +1,25 @@
 import avatar from "../assets/image-jeremy-small.png";
 import curve from "../assets/pattern-curved-line-2.svg";
-import blur from "../assets/pattern-blur.svg";
 
 export default function Testimonial() {
   return (
     <>
       <div className="testimonial">
         <div className="testimonial__avatar">
-          <img src={blur} alt="Blur" className="testimonial__avatar--blur" />
           <img
             src={avatar}
             alt="Fylo CMO"
             className="testimonial__avatar--img"
           />
         </div>
-        <div className="testimonial__curve">
-          <img
-            src={curve}
-            alt="Curved line"
-            className="testimonial__curve--img"
-          />
-        </div>
         <div className="testimonial__content">
+          <div className="testimonial__content--curve">
+            <img
+              src={curve}
+              alt="Curved line"
+              className="testimonial__content--curve-img"
+            />
+          </div>
           <h2 className="testimonial__content--headline">
             It just <span>works.</span>
           </h2>
@@ -31,7 +29,7 @@ export default function Testimonial() {
             job. This thing is a miracle worker.”
           </p>
           <p className="testimonial__content--name">Jeremy Robinson</p>
-          <p className="testimonial__content--position">CMO, Fylo</p>
+          <p className="testimonial__content--position">CMO, FYLO</p>
         </div>
       </div>
     </>
